483dd3171af9e5fb01523a4f12fe42802a9ab1bb
ho3einHamidi <hosseinmhh@yahoo.com>
2024-05-11 17:03:26 +0330

    feat: form logic

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,16 +1,37 @@
 import styles from "./Form.module.css";
 import Input from "../Input/Input";
+import { useState } from "react";
 function Form() {
+  const [data, setData] = useState({
+    name: null,
+    lastName: null,
+    email: null,
+    phone: null,
+  });
+  const onChangeInput = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+  const buttonHanlder = () => {
+    console.log(data);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.formWrapper}>
         <div className={styles.inputWrapper}>
-          <Input placeHolder="Name" />
-          <Input placeHolder="Last Name" />
-          <Input placeHolder="Email" />
-          <Input placeHolder="Phone" />
+          <Input name="name" onChange={onChangeInput} placeHolder="Name" />
+          <Input
+            name="lastName"
+            onChange={onChangeInput}
+            placeHolder="Last Name"
+          />
+          <Input name="email" onChange={onChangeInput} placeHolder="Email" />
+          <Input name="phone" onChange={onChangeInput} placeHolder="Phone" />
         </div>
-        <button className={styles.button}>Add Contact</button>
+        <button onClick={buttonHanlder} className={styles.button}>
+          Add Contact
+        </button>
       </div>
     </div>
   );
--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,7 +1,14 @@
 import PropTypes from "prop-types";
 import styles from "./Input.module.css";
-function Input({ placeHolder }) {
-  return <input className={styles.input} placeholder={placeHolder} />;
+function Input({ placeHolder, onChange, name }) {
+  return (
+    <input
+      name={name}
+      onChange={(event) => onChange(event)}
+      className={styles.input}
+      placeholder={placeHolder}
+    />
+  );
 }
 Input.propTypes = {
   placeHolder: PropTypes.string,
